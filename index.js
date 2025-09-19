d2907b47a50c5d32626bfea4730993c9032f2965
phantomlaww <lawrenceudeagbala@gmail.com>
2025-09-19 01:22:01 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,274 @@
+const products = [{
+    image: 'pics/2.jpeg',
+    name: 'STRAIGHT WIG'
+}, {
+ image: 'pics/3.jpeg',
+    name: 'CURLY WIG'
+}, {
+
+     image: 'pics/4.jpeg',
+    name: 'STRAIGHT WIG'
+}, {
+     image: 'pics/5.jpeg',
+    name: 'STRAIGHT WIG'
+}, {
+     image: 'pics/6.jpeg',
+    name: 'CURLY WIG'
+}, {
+     image: 'pics/7.jpeg',
+    name: 'CURLY WIG'
+}, {
+     image: 'pics/8.jpeg',
+    name: 'STRAIGHT WIG'
+}, {
+     image: 'pics/9.jpeg',
+    name: 'CURLY WIG'
+}, {
+     image: 'pics/10.jpeg',
+    name: 'STRAIGHT WIG'
+}, {
+     image: 'pics/11.jpeg',
+    name: 'STRAIGHT WIG'
+}, {
+     image: 'pics/12.jpeg',
+    name: 'STRAIGHT WIG'
+}, {
+     image: 'pics/13.jpeg',
+    name: 'STRAIGHT WIG'  
+    }, {
+     image: 'pics/14.jpeg',
+    name: 'STRAIGHT WIG'
+    }, {
+     image: 'pics/15.jpeg',
+    name: 'STRAIGHT WIG' 
+    }, {
+     image: 'pics/16.jpeg',
+    name: 'CURLY WIG'  
+    }, {
+     image: 'pics/17.jpeg',
+    name: 'CURLY WIG' 
+    }, {
+     image: 'pics/18.jpeg',
+    name: 'CURLY WIG'
+    }, {
+     image: 'pics/19.jpeg',
+    name: 'CURLY WIG'  
+    }, {
+     image: 'pics/20.jpeg',
+    name: 'CURLY WIG'
+    }, {
+     image: 'pics/21.jpeg',
+    name: 'CURLY WIG'
+    }, {
+     image: 'pics/22.jpeg',
+    name: 'STRAIGHT WIG'
+    }, {
+     image: 'pics/23.jpeg',
+    name: 'STRAIGHT WIG'
+    }, {
+     image: 'pics/24.jpeg',
+    name: 'STRAIGHT WIG'
+    }, {
+     image: 'pics/25.jpeg',
+    name: 'STRAIGHT WIG'
+    }, {
+     image: 'pics/26.jpeg',
+    name: 'CURLY WIG'
+    }, {
+     image: 'pics/27.jpeg',
+    name: 'STRAIGHT WIG'
+    }, {
+     image: 'pics/28.jpeg',
+    name: 'STRAIGHT WIG'
+    }, {
+     image: 'pics/29.jpeg',
+    name: 'STRAIGHT WIG'
+    }, {
+     image: 'pics/30.jpeg',
+    name: 'STRAIGHT WIG'
+    }, {
+     image: 'pics/31.jpeg',
+    name: 'STRAIGHT WIG'
+    }, {
+     image: 'pics/32.jpeg',
+    name: 'STRAIGHT WIG'
+    }, {
+     image: 'pics/33.jpeg',
+    name: 'CURLY WIG'
+    }, {
+     image: 'pics/34.jpeg',
+    name: 'STRAIGHT WIG'
+    }, {
+     image: 'pics/35.jpeg',
+    name: 'CURLY WIG'
+    }, {
+     image: 'pics/36.jpeg',
+    name: 'CURLY WIG'
+    }, {
+     image: 'pics/37.jpeg',
+    name: 'STRAIGHT WIG'
+    }, {
+     image: 'pics/38.jpeg',
+    name: 'STRAIGHT WIG'
+    }, {
+     image: 'pics/39.jpeg',
+    name: 'STRAIGHT WIG'
+    }, {
+     image: 'pics/40.jpeg',
+    name: 'CURLY WIG'
+    }, {
+     image: 'pics/41.jpeg',
+    name: 'CURLY WIG'
+    }, {
+     image: 'pics/42.jpeg',
+    name: 'CURLY WIG'
+    }, {
+     image: 'pics/43.jpeg',
+    name: 'CURLY WIG'
+    }, {
+     image: 'pics/44.jpeg',
+    name: 'CURLY WIG'
+    }, {
+     image: 'pics/45.jpeg',
+    name: 'CURLY WIG'
+    }, {
+     image: 'pics/46.jpeg',
+    name: 'STRAIGHT WIG'
+    }, {
+     image: 'pics/47.jpeg',
+    name: 'STRAIGHT WIG'
+    }, {
+     image: 'pics/48.jpeg',
+    name: 'CURLY WIG'
+    }, {
+     image: 'pics/49.jpeg',
+    name: 'STRAIGHT WIG'
+    }, {
+     image: 'pics/50.jpeg',
+    name: 'STRAIGHT WIG'
+    }, {
+     image: 'pics/51.jpeg',
+    name: 'CURLY WIG'
+    }, {
+     image: 'pics/52.jpeg',
+    name: 'STRAIGHT WIG'
+    }, {
+     image: 'pics/53.jpeg',
+    name: 'STRAIGHT WIG'
+    }, {
+     image: 'pics/54.jpeg',
+    name: 'HUMAN HAIR'
+    }, {
+     image: 'pics/55.jpeg',
+    name: 'STRAIGHT WIG'
+    }, {
+     image: 'pics/56.jpeg',
+    name: 'STRAIGHT WIG'
+    }, {
+     image: 'pics/57.jpeg',
+    name: 'HUMAN HAIR'
+    }, {
+     image: 'pics/58.jpeg',
+    name: 'STRAIGHT WIG'
+    }, {
+     image: 'pics/59.jpeg',
+    name: 'STRAIGHT WIG'
+    }, {
+     image: 'pics/60.jpeg',
+    name: 'CURLY WIG'
+    }, {
+     image: 'pics/61.jpeg',
+    name: 'CURLY WIG'
+    }, {
+     image: 'pics/62.jpeg',
+    name: 'CURLY WIG'
+    }, {
+     image: 'pics/63.jpeg',
+    name: 'CURLY WIG'
+    }, {
+     image: 'pics/64.jpeg',
+    name: 'STRAIGHT WIG'
+    }, {
+     image: 'pics/65.jpeg',
+    name: 'STRAIGHT WIG'
+    }, {
+     image: 'pics/66.jpeg',
+    name: 'CURLY WIG'
+    }, {
+     image: 'pics/67.jpeg',
+    name: 'CURLY WIG'
+    }, {
+     image: 'pics/68.jpeg',
+    name: 'CURLY WIG'
+    }, {
+     image: 'pics/69.jpeg',
+    name: 'CURLY WIG'
+    }, {
+     image: 'pics/70.jpeg',
+    name: 'STRAIGHT WIG'
+    }, {
+     image: 'pics/71.jpeg',
+    name: 'CURLY WIG'
+    }, {
+     image: 'pics/72.jpeg',
+    name: 'STRAIGHT WIG'
+    }, {
+     image: 'pics/73.jpeg',
+    name: 'CURLY WIG'
+    }, {
+     image: 'pics/74.jpeg',
+    name: 'STRAIGHT WIG'
+    }, {
+     image: 'pics/75.jpeg',
+    name: 'CURLY WIG'
+    }, {
+     image: 'pics/76.jpeg',
+    name: 'CURLY WIG'
+    }, {
+     image: 'pics/77.jpeg',
+    name: 'CURLY WIG'
+    }, {
+     image: 'pics/78.jpeg',
+    name: 'CURLY WIG'
+    }, {
+     image: 'pics/79.jpeg',
+    name: 'CURLY WIG'
+    }, {
+     image: 'pics/80.jpeg',
+    name: 'STRAIGHT WIG'
+    }, {
+     image: 'pics/81.jpeg',
+    name: 'CURLY WIG'
+    }, {
+     image: 'pics/82.jpeg',
+    name: 'STRAIGHT WIG'
+    }, {
+     image: 'pics/83.jpeg',
+    name: 'STRAIGHT WIG'
+    }, {
+     image: 'pics/84.jpeg',
+    name: 'STRAIGHT WIG'
+    }, {
+     image: 'pics/85.jpeg',
+    name: 'STRAIGHT WIG'
+    
+    }, {
+     image: 'pics/11.jpeg',
+    name: 'pics/11.jpeg'
+}];
+let productsHTML = '';
+
+products.forEach((product) => {
+productsHTML += `<div class="product">
+      <img src="${product.image}" alt="Straight Wig">
+      <h3>${product.name}</h3>
+      
+      <a href="#" class="btn" onclick="buyOnWhatsApp('Straight Wig', '30000')">Buy via WhatsApp</a>
+    </div>`;
+    
+});
+
+console.log(productsHTML);
+
+
+document.querySelector('.law').innerHTML = productsHTML;
